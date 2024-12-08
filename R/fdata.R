b357229301ca14b344f522fa086e76ac292c0eea
ALIMALII <alifadhaa16@gmail.com>
2024-12-08 19:04:18 +0300

    Add files via upload

---
+++ R/fdata.R
@@ -0,0 +1,18 @@
+#' Data to show something
+#'
+#' Contains the important data
+#'
+#'@format data frame with 100 rows and 3 variables
+#'
+#' \describe{
+#'     \item{the first col}{Y}
+#'     \item{the second}{X1}
+#'     \item{the third col}{X2}
+#'     }
+#'
+#'@source {create the dataset}
+#'
+#'@examples
+#' data(fdata)
+#'
+"fdata"
